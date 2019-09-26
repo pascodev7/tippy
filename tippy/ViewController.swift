40c358d332ea3acc410254ee64a5418854131ff7
Pascodev7 <Adonai#7>
2019-09-26 04:41:09 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by mac on 9/26/19.
+//  Copyright © 2019 Pascodev7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
